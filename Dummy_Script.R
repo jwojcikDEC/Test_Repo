bb14d9ae941640de7cbf542ac818e6741d019cb2
jwojcikDEC <jacob.wojcik@dec.ny.gov>
2023-02-16 14:00:04 -0500

    Initial Commit of Dummy Script
    Mainly intended to test the connection.

---
+++ Dummy_Script.R
@@ -0,0 +1,5 @@
+#' The sole purpose of this script is to make edits to test the push/pull connection
+#' with GitHub anytime a semi/major change is down locally that may influence 
+#' the connection.
+
+print("Hello World")
